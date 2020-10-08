8442eebd793889b7de07cbc53ff1c58d6e490e7b
sundar-0 <sundardumre69@gmail.com>
2020-10-08 19:53:35 +0545

    Updates

---
+++ precache-manifest.029f7eef4283c0c6cd1f73e8e4920f64.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c63c0d75540c49d748e6af6728daac3c",
+    "url": "/covid19Tracker/index.html"
+  },
+  {
+    "revision": "c2da0d5c8fc1952b5d4d",
+    "url": "/covid19Tracker/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "bf1c541c8b2a2bd9f85b",
+    "url": "/covid19Tracker/static/css/main.ecb3ebc1.chunk.css"
+  },
+  {
+    "revision": "c2da0d5c8fc1952b5d4d",
+    "url": "/covid19Tracker/static/js/2.d569d6a4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/covid19Tracker/static/js/2.d569d6a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf1c541c8b2a2bd9f85b",
+    "url": "/covid19Tracker/static/js/main.cf49cc53.chunk.js"
+  },
+  {
+    "revision": "be56bb067db5307bbefa",
+    "url": "/covid19Tracker/static/js/runtime-main.df36f96d.js"
+  }
+]);
